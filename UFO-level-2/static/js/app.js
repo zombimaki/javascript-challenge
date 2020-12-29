758d2b953ea698c39b30ffe5d4c6b2fc8116d182
zombimaki <andrewbperez@gmail.com>
2020-12-29 17:12:40 -0600

    tuesday start

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,49 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Set table body reference
+var tbody = d3.select("tbody");
+
+// Loop Through ufo sighting data and append it to the table
+tableData.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+  
+// select the filter button
+var button = d3.select("#filter-btn");
+
+// set on click behavior 
+button.on("click", runDateFilter);
+
+// date filter function
+function runDateFilter() {
+
+    //Clear the body
+    tbody.html("");
+
+    // Select the datetime input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the datetime value property of the input element
+    var inputValue = inputElement.property("value");
+
+    // Filter the data on datetime
+    var filteredData = tableData.filter(ufoSighting => ufoSighting.datetime === inputValue);
+
+    // Loop Through the filtered ufo sighting data and append it to the table
+    filteredData.forEach(function(filteredUFO) {
+
+    var row = tbody.append("tr");
+    Object.entries(filteredUFO).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+        });
+    });
+
+}
+
+
