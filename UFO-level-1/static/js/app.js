32a26e9f6199178bc90fd361f68a915b0d76261d
zombimaki <andrewbperez@gmail.com>
2020-12-28 19:17:07 -0600

    monday

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,43 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Set table body reference
+var tbody = d3.select("tbody");
+
+// Console.log the ufo data from data.js
+console.log(data);
+
+// // Loop Through `data` and console.log each ufo report object
+data.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+  
+// select the filter button
+var button = d3.select("#filter-btn");
+
+// Create event handlers 
+button.on("click", runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+  
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+  
+    console.log(inputValue);
+    console.log(tableData);
+  
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+  
+    console.log(filteredData);
+}
