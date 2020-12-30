fa6d320ce16c41a18a82649ca1a7ca2c445c0ac2
zombimaki <andrewbperez@gmail.com>
2020-12-29 18:23:35 -0600

    multipass

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -17,22 +17,51 @@ tableData.forEach((ufoReport) => {
 var button = d3.select("#filter-btn");
 
 // set on click behavior 
-button.on("click", runDateFilter);
+button.on("click", runFilter);
 
 // date filter function
-function runDateFilter() {
+function runFilter() {
 
     //Clear the body
     tbody.html("");
 
-    // Select the datetime input element and get the raw HTML node
-    var inputElement = d3.select("#datetime");
+    // Select the input elements and get the raw HTML node
+    var inputDate = d3.select("#datetime");
+    var inputCity = d3.select("#city");
+    var inputState = d3.select("#state");
+    var inputCountry = d3.select("#country");
+    var inputShape = d3.select("#shape");
 
     // Get the datetime value property of the input element
-    var inputValue = inputElement.property("value");
+    var inputDateValue = inputDate.property("value");
+    var inputCityValue = inputCity.property("value").toLowerCase();
+    var inputStateValue = inputState.property("value").toLowerCase();
+    var inputCountryValue = inputCountry.property("value").toLowerCase();
+    var inputShapeValue = inputShape.property("value").toLowerCase();
+    
+    // filter the table data where the input values exists
+    var filteredData = tableData;
 
-    // Filter the data on datetime
-    var filteredData = tableData.filter(ufoSighting => ufoSighting.datetime === inputValue);
+    if (inputDateValue) {
+        filteredData = filteredData.filter(ufoSighting => ufoSighting.datetime === inputDateValue);
+    }
+
+    if (inputCityValue) {
+        filteredData = filteredData.filter(ufoSighting => ufoSighting.city === inputCityValue);
+    }
+
+    if (inputStateValue) {
+        filteredData = filteredData.filter(ufoSighting => ufoSighting.state === inputStateValue);
+    }
+
+    if (inputCountryValue) {
+        filteredData = filteredData.filter(ufoSighting => ufoSighting.country === inputCountryValue);
+    }  
+   
+    if (inputShapeValue) {
+        filteredData = filteredData.filter(ufoSighting => ufoSighting.shape === inputShapeValue);
+    }
+                                        
 
     // Loop Through the filtered ufo sighting data and append it to the table
     filteredData.forEach(function(filteredUFO) {
